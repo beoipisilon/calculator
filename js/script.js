1895d3f2f261322193685be46f31bebe91035ab3
boy <48164586+beoipisilon@users.noreply.github.com>
2024-05-23 13:07:24 -0400

    feat(js): append to display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function clearDisplay() {
     document.getElementById('display').innerText = '0';
 }
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+      display.innerText = value;
+    } else {
+      display.innerText += value;
+    }
+}
